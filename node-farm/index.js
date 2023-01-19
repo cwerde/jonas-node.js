1f93aaebc5cf43b99b0f2e9f555d2cd3ad6a2869
Uğur Sucu <70368982+cwerde@users.noreply.github.com>
2023-01-19 11:34:30 +0300

    8. Reading and Writing Files

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
 
-const hello = 'Hello World!';
-console.log(hello);
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date(
+  Date.now()
+).toString()}`;
+
+fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
+console.log('The file has been written!');
